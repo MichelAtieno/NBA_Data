e70369da3ffcf76d3b68851eaf166517844a42c3
MichelAtieno <michelatieno23@gmail.com>
2023-04-14 18:11:49 +0300

    Added necessary templates to display team and player information

--- nba_data_app/urls.py
+++ nba_data_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import  home, team_data, active_player_data, scoreboard_data , player_profile, team_profile, box_score_data
+from .views import  home, team_data, active_player_data, scoreboard_data , player_profile, team_profile, box_score_data, get_one_season_playergamelog, get_all_seasons_player_gamelog, get_player_dashboard
 
 # get_one_season_player, get_all_seasons_player, 
 
@@ -9,8 +9,9 @@ urlpatterns = [
     path('', home, name="home"),
     path('all_teams', team_data, name="team_data"),
     path('active_players', active_player_data, name="active_player_data"),
-    # path('player/<id>&<season>', get_one_season_player, name="get_one_season_player"),
-    # path('player/<id>', get_all_seasons_player, name="get_all_seasons_player"),
+    path('player/<id>&<season>', get_one_season_playergamelog, name="get_one_season_playergamelog"),
+    path('all_seasons/<id>', get_all_seasons_player_gamelog, name="get_all_seasons_playergamelog"),
+    path('dashboard/<id>', get_player_dashboard, name="player_dashboard"),
     path('player/<id>', player_profile, name="player_profile"),
     path('team/<id>', team_profile, name="team_profile"),
     path('game/<id>', box_score_data, name="box_score_data"),
